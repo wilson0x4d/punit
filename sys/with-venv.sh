d009bf4b3b607c5ed46d75312c3ab036717356cb
wilson0x4d <mrshaunwilson@msn.com>
2025-01-24 12:15:58 -0600

    refactor(sys): `with-venv.sh` spawns new shell

--- sys/with-venv.sh
+++ sys/with-venv.sh
@@ -2,17 +2,12 @@
 # SPDX-FileCopyrightText: © 2024 Shaun Wilson
 # SPDX-License-Identifier: MIT
 #
-# use this script to drop into a venv, this
-# is designed to be used as an --rcfile target
-# for containers, or for manual/direct use.
+# use this script to drop into a venv.
 #
-# this script needs to be sourced or rc'd into
-# the shell to work correctly, fx:
-#
-# source sys/with-venv.sh
+# sys/with-venv.sh
 #
 ##
 source ~/.bashrc
 export PS1='\$ '
 echo -n -e "\033]0;pUnit\007"
-source .venv-bash/bin/activate
+bash --rcfile .venv-bash/bin/activate
