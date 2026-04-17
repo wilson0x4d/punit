77aaf98c60ec5eeae496c0438d2d8b4209ba4c4f
wilson0x4d <mrshaunwilson@msn.com>
2026-04-17 06:34:16 -0500

    chore: upgrade build workflow
    - new builder.
    - conforming assets.

--- sys/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: © Shaun Wilson
-# SPDX-License-Identifier: MIT
-##
-set -eo pipefail
-source .venv-bash/bin/activate
-python -m punit --trait '!integration' --report html --output results.html
