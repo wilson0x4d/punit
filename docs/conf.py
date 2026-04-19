5ca69c78ff276e9d000f919d7ecbf53cbd147770
wilson0x4d <mrshaunwilson@msn.com>
2026-04-19 18:50:53 -0500

    docs: theme switch.

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_theme_options = {
     'analytics_anonymize_ip': False,
