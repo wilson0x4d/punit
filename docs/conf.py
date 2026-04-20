c91464593e08f1d4998232aed806c90239a65e4d
wilson0x4d <mrshaunwilson@msn.com>
2026-04-19 19:08:45 -0500

    docs: backing out `furo`
    - sloppy, not a "drop in", requires more attention than it's worth.

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,14 @@ import punit
 project = 'pUnit'
 copyright = '2024 Shaun Wilson'
 author = 'Shaun Wilson'
-release = '0.0.0'
+release = '1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
@@ -29,7 +30,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_options = {
     'analytics_anonymize_ip': False,
